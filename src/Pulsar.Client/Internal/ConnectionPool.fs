51ce588ffd0000d78d8e080c469c1e97d75d82cc
Zike Yang <zike@apache.org>
2025-04-03 10:54:51 +0800

    Avoid setting the pauseWriterThreshold

--- src/Pulsar.Client/Internal/ConnectionPool.fs
+++ src/Pulsar.Client/Internal/ConnectionPool.fs
@@ -134,7 +134,7 @@ type internal ConnectionPool (config: PulsarClientConfiguration) =
                                   broker, maxMessageSize)
         backgroundTask {
             let (PhysicalAddress physicalAddress) = broker.PhysicalAddress
-            let pipeOptions = PipeOptions(pauseWriterThreshold = int64 maxMessageSize )
+            let pipeOptions = PipeOptions()
             let! socket = getSocket physicalAddress
 
             try
